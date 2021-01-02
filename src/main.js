5c11f72f8b5d45db38e15ef958f8a4722bcff8c3
mtumiati <manuel.tumiati@knobs.it>
2021-01-02 15:02:41 +0100

    Added and configured VueI18n package

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import VueClipboard from 'vue-clipboard2';
 import vClickOutside from 'v-click-outside';
 import config from '@/config/config';
 import Database from '@/classes/database/Database.ts';
+import VueI18n from 'vue-i18n';
 
+import i18nInit from './utils/i18n';
 import App from './App';
 import router from './router';
 import store from './store';
@@ -13,6 +15,7 @@ Vue.config.productionTip = false;
 
 Vue.use(VueClipboard);
 Vue.use(vClickOutside);
+Vue.use(VueI18n);
 
 sync(store, router);
 
@@ -24,8 +27,8 @@ window.Vault74 = {
       // eslint-disable-next-line no-console
       console.log(
         `%c [Vault74 Debug]: ${args[0]}`,
-        'color: #9b59b6; font-weight: bold; font-family: \'Major Mono Display\', monospace;',
-        ...args.slice(1, args.length),
+        "color: #9b59b6; font-weight: bold; font-family: 'Major Mono Display', monospace;",
+        ...args.slice(1, args.length)
       );
     }
   },
@@ -34,8 +37,8 @@ window.Vault74 = {
       // eslint-disable-next-line no-console
       console.log(
         `%c [Vault74 Warn]: ${args[0]}`,
-        'color: #e67e22; font-weight: bold; font-family: \'Major Mono Display\', monospace;',
-        ...args.slice(1, args.length),
+        "color: #e67e22; font-weight: bold; font-family: 'Major Mono Display', monospace;",
+        ...args.slice(1, args.length)
       );
     }
   },
@@ -43,10 +46,10 @@ window.Vault74 = {
     // eslint-disable-next-line no-console
     console.log(
       `%c [Vault74 Error]: ${args[0]}`,
-      'color: #e74c3c; font-weight: bold; font-family: \'Major Mono Display\', monospace;',
-      ...args.slice(1, args.length),
+      "color: #e74c3c; font-weight: bold; font-family: 'Major Mono Display', monospace;",
+      ...args.slice(1, args.length)
     );
-  },
+  }
   /* eslint-enable */
 };
 
@@ -59,8 +62,9 @@ new Vue({
   functional: true,
   router,
   store,
+  i18n: i18nInit('en_US'),
   render(h) {
     return h(App);
-  },
+  }
 });
 /* eslint-enable */
