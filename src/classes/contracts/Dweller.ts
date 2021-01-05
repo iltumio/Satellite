303bbdf2f455dd613403f29168e7cdf378c19b3e
RetroPronghorn <retropronghorn@gmail.com>
2021-01-05 01:48:50 -0500

    feat(Friends): Started work on friends helper contract

--- src/classes/contracts/Dweller.ts
+++ src/classes/contracts/Dweller.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
-import DwellerContract from '../utils/DwellerContract';
+import DwellerContract from '../../utils/DwellerContract';
 
 export default class Dweller {
   contractAddress: string;
---
+++ src/classes/contracts/Friends.ts
@@ -0,0 +1,54 @@
+import { Contract } from "web3-eth-contract";
+// @ts-ignore
+import * as FriendsInterface from '@/contracts/interfaces/Friends.json';
+// @ts-ignore
+import Ethereum from '@/classes/Ethereum';
+
+export class Friends {
+  ethereum: any;
+  contract: Contract;
+
+  constructor(address: string) {
+    this.ethereum = new Ethereum('window');
+    this.contract = this.getContract(address);
+  }
+
+  /** @function
+   * @name getContract
+   * @argument address Address of the DwellerID contract
+   * @returns contract instance ready for method execution
+   */
+  getContract(address: string) {
+    let contract: Contract;
+    contract = this.ethereum.getContract(FriendsInterface.abi, address);
+    return contract;
+  }
+
+  // Update Friends cache
+
+  // Update Friend requests recieved cache
+  async getRequests(account: string) {
+    const requests = await this.contract.methods
+      .getRequests()
+      .call({
+        from: account,
+      });
+    return requests;
+  }
+
+  async getRequest(id: number) {
+    const request = await this.contract.methods
+      .getRequest(id)
+      .call();
+    return request;
+  }
+  // Update Friend requests sent cache
+  async getFriends(account: string) {
+    const friends = await this.contract.methods
+      .getFriends()
+      .call({
+        from: account,
+      });
+    return friends;
+  }
+}
