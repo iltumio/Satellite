df618c76d88bc70250f654ae6e8284179d544c0f
RetroPronghorn <retropronghorn@gmail.com>
2021-01-28 14:15:39 -0500

    fix(Calling): Prevent glitches when calling and switching chats.

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Check for node
+if which node > /dev/null
+  then
+      echo "Node is installed..."
+  else
+      echo "Please install Nodejs"
+      exit 1
+  fi
+
+# Install yarn
+if yarn -v > /dev/null
+  then
+      echo "Yarn is installed..."
+  else
+      npm i -g yarn
+  fi
+
+# Pull Submodules
+git submodule update --init --recursive
+
+# Install Deps
+yarn
