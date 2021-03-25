635edf9a7f264d0f011f644e501e87bfbcf6fa1f
Jason Panay <jasonpanay@gmail.com>
2021-03-25 06:37:54 -0400

    Remove friends feature (1 bug with contract left)

--- src/classes/webrtc/WebRTC.ts
+++ src/classes/webrtc/WebRTC.ts
@@ -227,7 +227,7 @@ export default class WebRTC extends WebRTCMedia {
       }
     });
   }
- 
+
   connect(conn: Peer.DataConnection) {
     const identifier = this.buildIdentifier(conn.peer);
     const exitingPeer = this.find(identifier);
@@ -243,6 +243,18 @@ export default class WebRTC extends WebRTCMedia {
       peer.bind(conn);
       this.registerPeer(identifier, peer);
     });
+  }
+
+  public disconnectFromPeer(_identifier: string) : Promise<Error|null> {
+    return new Promise((resolve, reject) => {
+      if (!this.peer) reject(new Error('You cannot disconnect before initalizing.'))
+      const peer = this.find(_identifier);
+      if (peer) {
+        peer.close();
+        resolve(null);
+      }
+      setTimeout(() =>{ resolve(null) }, 3000)
+    })
   }
 
   public connectIfNotConnected(_identifier: string) {
