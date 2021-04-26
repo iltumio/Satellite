2f95c1b48dab27e707229d23baf1977fa76b2fd6
RetroPronghorn <retropronghorn@gmail.com>
2021-04-25 22:39:46 -0400

    version bump

--- src/config/config.js
+++ src/config/config.js
@@ -103,7 +103,6 @@ module.exports = {
     position: 'top-center', iconPack: 'fontawesome', duration: 800, className: 'vault-toast'
   },
   verified_addresses: [
-    '0xEE81E0B011Cd2D36D03F3E1C0A4549f78a56cb5E',
-    '0x4C5F7aFc2e263ef5eDe2849263399BF6FC4B6354'
+    '0x2c3a050a6634cBD10df8086688De37D084440B7c',
   ] // TODO: move this to a contract
 }
