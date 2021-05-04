457984a96411f153310e126717d76cf9d254f523
Kathy Chan <kathykemopaw@gmail.com>
2021-05-03 17:53:50 -0700

    minor edit

--- src/components/common/Badge.vue
+++ src/components/common/Badge.vue
@@ -77,5 +77,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less" src="./Badge.less">
-</style>
+<style scoped lang="less" src="./Badge.less"></style>
